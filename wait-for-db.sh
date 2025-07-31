b1e1d5ef905d7bc314bc34fda9057d9d722a4350
Chaaivisva <chaaivisva303@gmail.com>
2025-07-31 22:41:50 +0530

    Wait for db added

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -3,11 +3,14 @@
 
 set -e
 
+# The first argument to the script is the database host
 host="$1"
 shift
+# The rest of the arguments are the command to execute after the database is ready
 cmd="$@"
 
-until PGPASSWORD=$PGPASSWORD psql -h "$host" -U "$PGUSER" -d "$PGDATABASE" -c '\q'; do
+# Use a loop to keep trying to connect to the database
+until PGPASSWORD="$PGPASSWORD" psql -h "$host" -U "$PGUSER" -d "$PGDATABASE" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
