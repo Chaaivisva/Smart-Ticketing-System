fc92978027214c706a023d273cf4648e36e6b7d0
Chaaivisva <chaaivisva303@gmail.com>
2025-08-01 00:05:27 +0530

    toml file updated v1

--- smart_ticket/settings.py
+++ smart_ticket/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_fl%ote^fd9pu^(93$bsq
 DEBUG = os.environ.get('DEBUG_VALUE', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
-
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(',')
 
 # Application definition
 
