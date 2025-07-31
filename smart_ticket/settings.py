a355111c3e7f6bcc0126d1b588f75625583060cb
Chaaivisva <chaaivisva303@gmail.com>
2025-07-31 22:18:42 +0530

    Deploy to railways

--- smart_ticket/settings.py
+++ smart_ticket/settings.py
@@ -80,10 +80,14 @@ WSGI_APPLICATION = 'smart_ticket.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,  # 10 minutes
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("PGDATABASE"),
+        'USER': os.environ.get("PGUSER"),
+        'PASSWORD': os.environ.get("PGPASSWORD"),
+        'HOST': os.environ.get("PGHOST"),
+        'PORT': os.environ.get("PGPORT"),
+    }
 }
 
 
